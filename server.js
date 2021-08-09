840799d7a3dc777510b2e32dbd5703b089a572f9
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-09 13:02:52 +0800

    increment the number of entries

--- server.js
+++ server.js
@@ -81,7 +81,21 @@ app.get('/profile/:id', (req, res) => {
     }
 })
 
-
+//increment the number of entries
+app.post('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries); 
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+})
 
 
 
