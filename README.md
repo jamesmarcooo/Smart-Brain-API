729ef776fd4d54e30b42c5c8b2177286ce7023b2
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-09 13:02:16 +0800

    signin

---
+++ README.md
@@ -0,0 +1,1 @@
+Uses bcrypt-nodejs implementation on hash to have a strong password security.
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
+//setting up express.js
 const app = express();
+
+//setting up body parser for json
+app.use(bodyParser.json());
 
 const database = {
     users: [
@@ -20,15 +26,38 @@ const database = {
             entries: 0,
             joined: new Date()
         }
+    ],
+    login: [
+        {
+            id:'987',
+            hash:'',
+            email:'john@gmail.com'
+        }
     ]
 }
 
+//gets list of users @ route localhost:3000/
 app.get('/', (req, res) => {
-    res.send('this is working');
+    res.send(database.users);
 })
 
+//Register @ localhost:3000/register
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body //destructuring
+    database.users.push({ //template
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]); //response, if not included postman will just load
+})
+
+//Signing in @ localhost:3000/signin
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.users.[0].email && req.body.password === database.users[0].password){
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
         res.json("success")
     }else{
         res.status(400).json('error logging in')
@@ -36,6 +65,13 @@ app.post('/signin', (req, res) => {
     res.json('signing')
 })
 
+
+
+
+
+
+
+//setting of localhost:3000
 app.listen(3000, () => {
     console.log('app is running port 3000')
 }) 
