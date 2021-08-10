e9316687166015b804bc80638f4d35a07a457f19
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-10 08:14:35 +0800

    set up knex

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     })
@@ -82,7 +81,7 @@ app.get('/profile/:id', (req, res) => {
 })
 
 //increment the number of entries
-app.post('/image', (req, res) => {
+app.put('/image', (req, res) => {
     const { id } = req.body;
     let found = false;
     database.users.forEach(user => {
